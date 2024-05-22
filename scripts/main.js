f08717136a255d4fd516560fc42e91f2e4569780
PeriloJunior <perilojunior68@gmail.com>
2024-05-22 14:28:01 -0300

    Funcionalidade para direcionar a cidade pesquisada

--- scripts/main.js
+++ scripts/main.js
@@ -11,10 +11,34 @@ document.addEventListener('DOMContentLoaded', () => {
             zoom: 10
         })
     });
-
+    
     document.getElementById('consult').addEventListener('click', () => {
         const location = document.getElementById('location').value;
-        const option = document.getElementById('options').value;
-        console.log(`Location: ${location}, Option: ${option}`);
+        const apiKey = '97dceee882d90161667aebab74971875';
+
+        // URL da API do OpenWeather
+        const apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${location}&appid=${apiKey}`;
+        console.log(apiUrl);
+        // Faz a requisição à API usando fetch
+        fetch(apiUrl)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                const latitude = data[0].lat;
+                const longitude = data[0].lon;
+
+                map.getView().setCenter(ol.proj.fromLonLat([longitude, latitude]));
+                map.getView().setZoom(12);
+
+                console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+            })
+            .catch(error => {
+                console.error('There was a problem with the fetch operation:', error);
+            });
     });
+    
 });
