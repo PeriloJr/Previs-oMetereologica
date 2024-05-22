643e51475859971132c8987e59701904799d55e8
PeriloJunior <perilojunior68@gmail.com>
2024-05-22 20:35:52 -0300

    Refatoração e implementação de novas funcionalidades

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
+    class City {
+        constructor(name, lat, lon) {
+          this.name = name;
+          this.lat = lat;
+          this.lon = lon;
+        }
+      }
+    
+    const _moonIconUrl = "https://assets.hgbrasil.com/weather/icons/moon/";
+    const _weatherType = "https://assets.hgbrasil.com/weather/icons/conditions/";
+
     const layer = new ol.layer.Tile({
         source: new ol.source.OSM()
     });
@@ -12,33 +23,98 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     });
     
-    document.getElementById('consult').addEventListener('click', () => {
+    const BuildApiUrl = (isGeoLocationUrl) =>{
         const location = document.getElementById('location').value;
-        const apiKey = '97dceee882d90161667aebab74971875';
+        let apiUrl = "";
 
-        // URL da API do OpenWeather
-        const apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${location}&appid=${apiKey}`;
-        console.log(apiUrl);
-        // Faz a requisição à API usando fetch
-        fetch(apiUrl)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                const latitude = data[0].lat;
-                const longitude = data[0].lon;
+        if(isGeoLocationUrl){
+            const apiKey = '97dceee882d90161667aebab74971875';
+            apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${location}&appid=${apiKey}`;
+        }else{
+            apiUrl = `https://api.hgbrasil.com/weather?city_name=${location}`;
+        }
 
-                map.getView().setCenter(ol.proj.fromLonLat([longitude, latitude]));
-                map.getView().setZoom(12);
+        const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+        return proxyUrl + apiUrl;
+    }
 
-                console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
-            })
-            .catch(error => {
-                console.error('There was a problem with the fetch operation:', error);
-            });
-    });
+    const SetMapLocation = (latitude, longitude) =>{
+        map.getView().setCenter(ol.proj.fromLonLat([longitude, latitude]));
+        map.getView().setZoom(12);
+    }
+
+    const CallAPIInformations = async (apiUrl) => {
+        try {
+            const response = await fetch(apiUrl);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error('There was a problem with the fetch operation:', error);
+            throw error;
+        }
+    }
+
+    const OpenModalInformations = () =>{
+        var modal = document.getElementById("myModal");
+        modal.style.display = "block";
+        return modal
+    }
+
+    const FillDataInformations = (WeatherInformations) =>{
+        console.log(WeatherInformations.results);
+        const cityName = document.getElementById('cityName');
+        cityName.textContent = `Cidade: ${WeatherInformations.results.city}`;
+
+        const weatherType = document.getElementById('weatherTypeLabel');
+        weatherType.textContent = `Tipo do clima: `;
+
+        const weatherTypeIcon = document.getElementById('weatherTypeIcon');
+        weatherTypeIcon.src = `${_weatherType}${WeatherInformations.results.condition_slug}.svg`;
+
+        const maxTemperature = document.getElementById('maxTemperature');
+        maxTemperature.textContent = `Temperatura maxima: ${WeatherInformations.results.forecast[0].max} ºC`;
+
+        const minTemperature = document.getElementById('minTemperature');
+        minTemperature.textContent = `Temperatura minima: ${WeatherInformations.results.forecast[0].min} ºC`;
+
+        const moonFase = document.getElementById('moonPhaseLabel');
+        moonFase.textContent = `Fase da lua: `;
+
+        const moonPhaseIcon = document.getElementById('moonPhaseIcon');
+        moonPhaseIcon.src = `${_moonIconUrl}${WeatherInformations.results.moon_phase}.png`;
+    }
+
+    document.getElementById('consult').addEventListener('click', async () => {
+        const geoLocationUrl = BuildApiUrl(true);
+        const weatherUrl = BuildApiUrl(false);
+        try{
+            const CityInformations = await CallAPIInformations(geoLocationUrl);
+            const city = new City(CityInformations[0].name, CityInformations[0].lat, CityInformations[0].lon)
+            SetMapLocation(city.lat, city.lon);
+            
+            const WeatherInformations = await CallAPIInformations(weatherUrl);
+            
+            modal = OpenModalInformations();
+            
+            FillDataInformations(WeatherInformations);
+
+        }catch(error){
+            console.error('Error fetching city information:', error);
+        }
+
+        var span = document.getElementsByClassName("close")[0];
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
     
+        window.onclick = function(event) {
+            if (event.target == modal) {
+              modal.style.display = "none";
+            }}
+    });
+
+
 });
